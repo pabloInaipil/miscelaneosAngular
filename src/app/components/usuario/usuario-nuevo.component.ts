5d9b1e0f07e4047f9c51231c1538f2aa2a94b7d8
Lenovo <pablo.inaipil@gmail.com>
2021-06-10 02:03:17 -0400

    first commit

---
+++ src/app/components/usuario/usuario-nuevo.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+
+@Component({
+  selector: 'app-usuario-nuevo',
+  template: `
+    <p>
+      usuario-nuevo works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class UsuarioNuevoComponent implements OnInit {
+
+  constructor( private router:ActivatedRoute) { 
+        
+    this.router.parent.params.subscribe( parametros => {
+
+        console.log( "RUTA HIJA USUSARIO NEW")
+        console.log(parametros);
+    })
+  }
+
+
+  ngOnInit(): void {
+  }
+
+}
